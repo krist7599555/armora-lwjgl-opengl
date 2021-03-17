5acb668c0125d87871593d5a07ec959f84445184
krist7599555 <krist7599555@gmail.com>
2021-03-17 19:06:37 +0700

    refactor uniformLocation to GlParam.kt

--- src/GlParam.kt
+++ src/GlParam.kt
@@ -1,17 +1,22 @@
+import org.joml.Matrix3f
 import org.joml.Matrix4f
+import org.joml.Vector3f
 import org.lwjgl.BufferUtils
 import org.lwjgl.opengl.ARBShaderObjects.*
-import org.lwjgl.opengl.ARBVertexBufferObject.*
-import org.lwjgl.opengl.ARBVertexShader.*
-import org.lwjgl.opengl.GL11
 import java.nio.FloatBuffer
 
-class GlParam<T: Matrix4f> (val program: Int, val type: GlParam.Type, val name: String, var data: T, val size: Int) {
+class GlParam<T: Any> (val program: Int, val type: GlParam.Type, val name: String, var data: T) {
     enum class Type {
         UNIFORM,
         VERTEX_ATTRIBUTE
     }
-    private val address: Int
+    val address: Int
+    private val capacity: Int = when (data) {
+        is Matrix4f -> 16
+        is Matrix3f -> 9
+        is Vector3f -> 3
+        else -> throw Exception("no matching type")
+    }
     private val buffer: FloatBuffer
     init {
         address = when(type) {
@@ -19,26 +24,36 @@ class GlParam<T: Matrix4f> (val program: Int, val type: GlParam.Type, val name: 
 //            Type.VERTEX_ATTRIBUTE -> glGetAttribLocationARB(program, name).also { glEnableVertexAttribArrayARB(it) }
             else -> throw IllegalArgumentException("type $type not support")
         }
-        buffer = BufferUtils.createFloatBuffer(size)
+        buffer = BufferUtils.createFloatBuffer(capacity)
     }
     fun update() {
-        data.get(buffer);
-        when(type) {
-            Type.UNIFORM -> glUniformMatrix4fvARB(address, false, buffer)
-            else -> throw IllegalArgumentException("type $type not support")
-//            Type.VERTEX_ATTRIBUTE -> {
-//                glBindBufferARB(GL_ARRAY_BUFFER_ARB, buffer.address)
-//                glVertexAttribPointerARB(address, 3, GL11.GL_FLOAT, false, 0, 0)
-//            }
+        when (data) {
+            is Matrix4f -> (data as Matrix4f).get(buffer)
+            is Matrix3f -> (data as Matrix3f).get(buffer)
+            is Vector3f -> (data as Vector3f).get(buffer)
+            else -> throw Exception("no matching type")
         }
-
+    }
+    fun render() {
+        this.update()
+        if (type == Type.UNIFORM) {
+            when (data) {
+                is Matrix4f -> glUniformMatrix4fvARB(address, false, buffer)
+                is Matrix3f -> glUniformMatrix3fvARB(address, false, buffer)
+                is Vector3f -> glUniform3fvARB(address, buffer)
+                else -> throw Exception("no matching type")
+            }
+        } else {
+            throw IllegalArgumentException("type $type not support")
+        }
     }
 }
 
 
+
 fun main() {
     val program = 0
-    val param = GlParam(program, GlParam.Type.UNIFORM, "uVertex", Matrix4f(), 16)
+    val param = GlParam(program, GlParam.Type.UNIFORM, "uVertex", Matrix4f())
 //    Vector3f()
 //    ARBVertexBufferObject.glBindBufferARB(ARBVertexBufferObject.GL_ARRAY_BUFFER_ARB, mesh.vertexArrayBuffer)
 //    ARBVertexShader.glVertexAttribPointerARB(engine.aVertex, 3, GL11.GL_FLOAT, false, 0, 0)
--- src/ModelViewer.kt
+++ src/ModelViewer.kt
@@ -9,12 +9,20 @@ class ModelViewer(private var model: Model, private var engine: Demo) {
 
     fun render() {
         // setter buffer
-        engine.modelMatrix.get(engine.modelMatrixBuffer)
-        engine.viewProjectionMatrix.get(engine.viewProjectionMatrixBuffer)
-        engine.normalMatrix.set(engine.modelMatrix).invert().transpose()
-        engine.normalMatrix.get(engine.normalMatrixBuffer)
-        engine.lightPosition.get(engine.lightPositionBuffer)
-        engine.viewPosition.get(engine.viewPositionBuffer)
+//        engine.modelMatrix.get(engine.modelMatrixBuffer)
+        engine.uModelMatrix.render()
+//        engine.viewProjectionMatrix.get(engine.viewProjectionMatrixBuffer)
+        engine.uViewProjectionMatrix.render()
+//        engine.normalMatrix.set(engine.modelMatrix).invert().transpose()
+        engine.uNormalMatrix.data.set(engine.uModelMatrix.data).invert().transpose()
+        engine.uNormalMatrix.render()
+
+//        engine.normalMatrix.get(engine.normalMatrixBuffer)
+
+//        engine.lightPosition.get(engine.lightPositionBuffer)
+        engine.uLightPosition.render()
+//        engine.viewPosition.get(engine.viewPositionBuffer)
+        engine.uViewPosition.render()
 
         for (mesh in model.meshes) {
             // mesh.vertexArrayBuffer -> aVertex
@@ -25,19 +33,24 @@ class ModelViewer(private var model: Model, private var engine: Demo) {
             ARBVertexShader.glVertexAttribPointerARB(engine.aNormal, 3, GL11.GL_FLOAT, false, 0, 0)
 
             // engine.modelMatrixBuffer -> uModelMatrix
-            ARBShaderObjects.glUniformMatrix4fvARB(engine.uModelMatrix, false, engine.modelMatrixBuffer)
+//            ARBShaderObjects.glUniformMatrix4fvARB(engine.uModelMatrix, false, engine.modelMatrixBuffer)
+            engine.uModelMatrix.render()
 
             // engine.viewProjectionMatrixBuffer -> uViewProjectionMatrix
-            ARBShaderObjects.glUniformMatrix4fvARB(engine.uViewProjectionMatrix, false, engine.viewProjectionMatrixBuffer)
+//            ARBShaderObjects.glUniformMatrix4fvARB(engine.uViewProjectionMatrix, false, engine.viewProjectionMatrixBuffer)
+            engine.uViewProjectionMatrix.render()
 
-            ARBShaderObjects.glUniformMatrix3fvARB(engine.uNormalMatrix, false, engine.normalMatrixBuffer)
-            ARBShaderObjects.glUniform3fvARB(engine.uLightPosition, engine.lightPositionBuffer)
-            ARBShaderObjects.glUniform3fvARB(engine.uViewPosition, engine.viewPositionBuffer)
+//            ARBShaderObjects.glUniformMatrix3fvARB(engine.uNormalMatrix, false, engine.normalMatrixBuffer)
+            engine.uNormalMatrix.render()
+//            ARBShaderObjects.glUniform3fvARB(engine.uLightPosition, engine.lightPositionBuffer)
+            engine.uLightPosition.render()
+//            ARBShaderObjects.glUniform3fvARB(engine.uViewPosition, engine.viewPositionBuffer)
+            engine.uViewPosition.render()
 
             model.materials[mesh.material_index].also { material ->
-                ARBShaderObjects.nglUniform3fvARB(engine.uAmbientColor, 1, material.ambientColor.address())
-                ARBShaderObjects.nglUniform3fvARB(engine.uDiffuseColor, 1, material.diffuseColor.address())
-                ARBShaderObjects.nglUniform3fvARB(engine.uSpecularColor, 1, material.specularColor.address())
+                ARBShaderObjects.nglUniform3fvARB(engine.uAmbientColor.address, 1, material.ambientColor.address())
+                ARBShaderObjects.nglUniform3fvARB(engine.uDiffuseColor.address, 1, material.diffuseColor.address())
+                ARBShaderObjects.nglUniform3fvARB(engine.uSpecularColor.address, 1, material.specularColor.address())
             }
 
             ARBVertexBufferObject.glBindBufferARB(
